b70c560f51fd006d26526625f991690a0f537030
Abhyuday <abhyuday964808@gmail.com>
2018-09-14 17:08:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
 	
 	document.querySelector("form").onsubmit = ()=>{
 		var data=document.querySelector("element").value;
-		const request = new XMLHttpRequest(); // creating request object
-		request.open("POST","includes/signup.inc.php");
+		const request = new FormData();
+		request.open("method","includes/signup.inc.php");
                 request.onload = () => {
-                    var data =JSON.parse(request.responseText);
+                    var data =request.responseText;
                     console.log('data');
-                    // do what you want to do with data 
+                   
 
                     }
-                request.send(); // sending request
-                return false; // prevent from refreshing the page
+                request.send(); 
+                return false;
 }		
   
 })
